aff6bc5781cad7ac3cb8055767c04ca45c194614
Elly Lee <Elly890216@gmail.com>
2020-05-03 15:37:25 +0900

    first commit

---
+++ src/main/java/com/example/sbs/cuni/Cuni2Application.java
@@ -0,0 +1,13 @@
+package com.example.sbs.cuni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cuni2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cuni2Application.class, args);
+	}
+
+}
